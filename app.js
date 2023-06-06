2830b7f92bfda76c874bc52d0f76dfec4ca7c854
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-06-06 21:00:57 +0900

    index.js

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ var indexRouter = require('./routes/index');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: '3.34.200.80',
-  user : 'root',
-  password : '1234',
-  port : '50768',
-  database : 'ManageSys'
+    host: '54.180.104.62',
+    user: 'root',
+    password: '1234',
+    port: '59290',
+    database: 'ManageSys',
 });
 
 var app = express();
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('Error connecting to MySQL database: ' + err.stack);
-    return;
-  }
+connection.connect(function (err) {
+    if (err) {
+        console.error('Error connecting to MySQL database: ' + err.stack);
+        return;
+    }
 
-  console.log('Connected to MySQL database as id ' + connection.threadId);
+    console.log('Connected to MySQL database as id ' + connection.threadId);
 });
 
 // view engine setup
@@ -43,19 +43,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.sendFile(__dirname + '/KWAS/error.html');
+    // render the error page
+    res.status(err.status || 500);
+    res.sendFile(__dirname + '/KWAS/error.html');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'KWAS', 'main', 'index.html'));
+router.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '../KWAS/main/index.html'));
+    // res.sendFile(path.join(__dirname, '../KWAS/login_form/login.html'));
 });
 
 module.exports = router;
