d26bb71817a52ce4210c1a86cd30e39db1c3542e
JASON8917 <57662457+JASON8917@users.noreply.github.com>
2023-06-09 22:56:21 +0900

    index dump

--- models/indexModel.js
+++ models/indexModel.js
@@ -8,8 +8,9 @@ const conn = mysql.createConnection({
 });
 
 // Fetch the timetable data for a specific user
+userId = 'shine8917'
 exports.getTimetable = (userId) => {
-    /* return new Promise((resolve, reject) => {
+      return new Promise((resolve, reject) => {
       const query = 'SELECT lecture_name, lecture_week1, lecture_week2, lecture_time1, lecture_time2 FROM user_lecture AS ul, lecture AS l WHERE ul.user_id = ? AND ul.lecture_code = l.lecture_code';
   
       conn.query(query, userId, (err, rows) => {
@@ -19,15 +20,17 @@ exports.getTimetable = (userId) => {
           resolve(rows);
         }
       } );
-    });*/
+    });
+  };
 
+exports.getTimetable = () => {
     const data = {
         lecture_name: '소프트웨어공학',
         lecture_week1: '월요일',
         lecture_week2: '수요일',
         lecture_time1: '5',
         lecture_time2: '6'
-      };
-    
-      return data;
-  };
+    };
+
+    return data;
+};
