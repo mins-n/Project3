1e4471558219b9d8c15193195059d494e5f6208c
JASON8917 <57662457+JASON8917@users.noreply.github.com>
2023-06-09 22:39:05 +0900

    index little change

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,7 +4,7 @@ const indexModel = require('../models/indexModel');
 // Handle the GET request for the index page
 exports.getIndexPage = (req, res) => {
   // Call the corresponding function in the index model to fetch data
-  const data = indexModel.getData();
+  const data = indexModel.getTimetable();
 
   // Render the index view and pass the data
   res.render('index', { data });
