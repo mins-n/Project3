30547df83c6f8a43dbcc2cb632525e4af7d7fde2
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-06-09 23:43:18 +0900

    table
    main page 시간표 수정

--- models/indexModel.js
+++ models/indexModel.js
@@ -1,27 +1,27 @@
-const mysql = require('mysql');
-const conn = mysql.createConnection({
-  host: '54.180.104.62',
-  user: 'root',
-  password: '1234',
-  port: '58261',
-  database: 'ManageSys',
-});
+// const mysql = require('mysql');
+// const conn = mysql.createConnection({
+//   host: '54.180.104.62',
+//   user: 'root',
+//   password: '1234',
+//   port: '58261',
+//   database: 'ManageSys',
+// });
 
-// Fetch the timetable data for a specific user
-userId = 'shine8917'
-exports.getTimetable = (userId) => {
-      return new Promise((resolve, reject) => {
-      const query = 'SELECT lecture_name, lecture_week1, lecture_week2, lecture_time1, lecture_time2 FROM user_lecture AS ul, lecture AS l WHERE ul.user_id = ? AND ul.lecture_code = l.lecture_code';
-  
-      conn.query(query, userId, (err, rows) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(rows);
-        }
-      } );
-    });
-  };
+// // Fetch the timetable data for a specific user
+// userId = 'shine8917'
+// exports.getTimetable = (userId) => {
+//       return new Promise((resolve, reject) => {
+//       const query = 'SELECT lecture_name, lecture_week1, lecture_week2, lecture_time1, lecture_time2 FROM user_lecture AS ul, lecture AS l WHERE ul.user_id = ? AND ul.lecture_code = l.lecture_code';
+
+//       conn.query(query, userId, (err, rows) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(rows);
+//         }
+//       } );
+//     });
+//   };
 
 exports.getTimetable = () => {
     const data = {
@@ -29,7 +29,7 @@ exports.getTimetable = () => {
         lecture_week1: '월요일',
         lecture_week2: '수요일',
         lecture_time1: '5',
-        lecture_time2: '6'
+        lecture_time2: '6',
     };
 
     return data;
