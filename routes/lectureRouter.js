91fb599c8d2010ab951ed1041fd24b67b8a79f9f
MS5682 <92199689+MS5682@users.noreply.github.com>
2023-06-14 21:43:03 +0900

    lecture 수정

--- routes/lectureRouter.js
+++ routes/lectureRouter.js
@@ -3,8 +3,8 @@ var router = express.Router();
 const lectureController = require('../controllers/lectureController');
 
 router.get('/syllabus_inquiry', lectureController.getLecture);
-router.get('/enrolment', lectureController.getLecture);
-router.post('/enrolment', lectureController.enrolment);
+router.get('/enrolment', lectureController.getLecture2);
+//router.post('/enrolment', lectureController.enrolment);
 
 module.exports = router;
 
