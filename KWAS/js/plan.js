4c954325f1fe4ba52b6f5dbcdd798b4914ea32e0
9-ch <9ch@kakao.com>
2023-06-16 05:12:02 +0900

    fee, plan
    등록금, 강의계획서 내부 css, 스타일 적용

--- KWAS/js/plan.js
+++ KWAS/js/plan.js
@@ -1,24 +1,30 @@
 axios.get("/course_management/plan?lecture_code=" + lectureCode)
-      .then(function (response) {
-        var data = response.data;
-        var lectureInfo = data.lectureInfo[0];
-        var planList = data.planList;
+  .then(function (response) {
+    var data = response.data;
+    var lectureInfo = data.lectureInfo[0];
+    var planList = data.planList;
 
-        // Display lecture information
-        var lectureInfoElement = document.getElementById("lectureInfo");
-        lectureInfoElement.innerHTML = "<p><strong>Lecture Code:</strong> " + lectureInfo.lecture_code + "</p>" +
-          "<p><strong>Lecture Name:</strong> " + lectureInfo.lecture_name + "</p>" +
-          "<p><strong>Lecture Class:</strong> " + lectureInfo.lecture_class + "</p>" +
-          "<p><strong>Lecture Info:</strong> " + lectureInfo.lecture_info + "</p>";
+    // Display lecture information
+    var lectureInfoTable = document.getElementById("lectureInfoTable");
+    lectureInfoTable.innerHTML = ""; // Clear existing table content
 
-        // Display weekly plan
-        var planTableBody = document.querySelector("#planTable tbody");
-        planList.forEach(function (plan) {
-          var row = document.createElement("tr");
-          row.innerHTML = "<td>" + plan.week + "</td><td>" + plan.plan + "</td>";
-          planTableBody.appendChild(row);
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    var lectureInfoRows = "";
+    lectureInfoRows += "<tr><th>Lecture Code</th><td>" + lectureInfo.lecture_code + "</td></tr>";
+    lectureInfoRows += "<tr><th>Lecture Name</th><td>" + lectureInfo.lecture_name + "</td></tr>";
+    lectureInfoRows += "<tr><th>Lecture Class</th><td>" + lectureInfo.lecture_class + "</td></tr>";
+    lectureInfoRows += "<tr><th>Lecture Info</th><td>" + lectureInfo.lecture_info + "</td></tr>";
+    lectureInfoTable.innerHTML = lectureInfoRows;
+
+    // Display weekly plan
+    var planTableBody = document.querySelector("#planTable tbody");
+    planTableBody.innerHTML = ""; // Clear existing table rows
+
+    planList.forEach(function (plan) {
+      var row = document.createElement("tr");
+      row.innerHTML = "<td>" + plan.week + "</td><td>" + plan.plan + "</td>";
+      planTableBody.appendChild(row);
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
