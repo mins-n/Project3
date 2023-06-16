c3d8b99c6124e7b902b046b03701cf53dcab7fcf
MS5682 <92199689+MS5682@users.noreply.github.com>
2023-06-16 17:44:02 +0900

    아이디 비밀번호 찾기 수정

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -4,8 +4,8 @@ const loginController = require('../controllers/loginController');
 
 router.post('/register', loginController.join);
 router.post('/login', loginController.login);
-router.post('/forget_id', loginController.findId);
-router.post('/forget_password', loginController.findPw);
+router.get('/forget_id', loginController.findId);
+router.get('/forget_password', loginController.findPw);
 
 module.exports = router;
 
