cf0467192f8f0edee5630954e2216b7699a21433
MS5682 <92199689+MS5682@users.noreply.github.com>
2023-06-16 09:11:23 +0900

    Update listening_student.js

--- KWAS/js/listening_student.js
+++ KWAS/js/listening_student.js
@@ -1,5 +1,4 @@
-axios
-  .get("/user/professor/lecture/" + lectureCode)
+axios.get("/user/professor/lecture/" + lectureCode)
   .then(function (response) {
     var students = response.data;
 
@@ -8,7 +7,7 @@ axios
 
     var lectureInfoRows = "";
     students.forEach(function (student) {
-      lectureInfoRows += "<tr><td>학생 ID</td><td>" + student.user_id + "</td><td>이름</td><td>" + student.name + "</td><td>학점</td><td><input type='text' name='grade' id='grade_" + student.user_id + "' value='" + student.grade + "'></td><td><button onclick='saveGrade(" + student.user_id + ")'>입력</button></td></tr>";
+      lectureInfoRows += "<tr><td>학생 ID</td><td>" + student.user_id + "</td><td>이름</td><td>" + student.name + "</td><td>학점</td><td><input type='text' name='grade' id='grade_" + student.user_id + "' value='" + student.grade + "'></td><td><button onclick='saveGrade(" + JSON.stringify(student.user_id) + ", " + JSON.stringify(lectureCode) + ")'>입력</button></td></tr>";
     });
     lectureInfoTable.innerHTML = lectureInfoRows;
 
@@ -17,7 +16,7 @@ axios
 
     students.forEach(function (student) {
       var row = document.createElement("tr");
-      row.innerHTML = "<td>" + student.user_id + "</td><td>" + student.name + "</td><td><input type='text' name='grade' id='grade_" + student.user_id + "' value='" + student.grade + "'></td><td><button onclick='saveGrade(" + student.user_id + ")'>입력</button></td>";
+      row.innerHTML = "<td>" + student.user_id + "</td><td>" + student.name + "</td><td><input type='text' name='grade' id='grade_" + student.user_id + "' value='" + student.grade + "'></td><td><button onclick='saveGrade(" + JSON.stringify(student.user_id) + ", " + JSON.stringify(lectureCode) + ")'>입력</button></td>";
       planTableBody.appendChild(row);
     });
   })
@@ -25,10 +24,25 @@ axios
     console.log(error);
   });
 
-function saveGrade(userId) {
+function saveGrade(userId, lectureCode) {
   var gradeInput = document.getElementById("grade_" + userId);
   var gradeValue = gradeInput.value;
-  // 여기에서 학점 저장 로직을 구현하세요.
-  // 저장 버튼을 클릭했을 때 실행되는 함수입니다.
-  // userId와 gradeValue를 활용하여 학점을 저장하는 로직을 작성하세요.
+
+  // Prepare data to send
+  var data = {
+    user_id: userId,
+    lecture_code: lectureCode,
+    grade: gradeValue
+  };
+  console.log(data);
+  // Send data to the server using Axios
+  axios.post("/user/professor/lecture/score", data)
+    .then(function (response) {
+      // Handle the response from the server if needed
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // Handle errors if any
+      console.log(error);
+    });
 }
